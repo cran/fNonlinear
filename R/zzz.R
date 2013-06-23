d197cfc1de19f7475bf3aa05e1298fd36faff4b5
Yohan Chalabi <yohan.chalabi@rmetrics.org>
2013-06-23 00:00:00 +0000

    version 3010.78

--- R/zzz.R
+++ R/zzz.R
@@ -29,20 +29,20 @@
 ################################################################################
 
 
-.First.lib =
-function(lib, pkg)
-{
-    # Startup Mesage and Desription:
-    MSG <- if(getRversion() >= "2.5") packageStartupMessage else message
-    dsc <- packageDescription(pkg)
-    if(interactive() || getOption("verbose")) {
-        # not in test scripts
-        MSG(sprintf("Rmetrics Package %s (%s) loaded.", pkg, dsc$Version))
-    }
+## .First.lib =
+## function(lib, pkg)
+## {
+##     # Startup Mesage and Desription:
+##     MSG <- if(getRversion() >= "2.5") packageStartupMessage else message
+##     dsc <- packageDescription(pkg)
+##     if(interactive() || getOption("verbose")) {
+##         # not in test scripts
+##         MSG(sprintf("Rmetrics Package %s (%s) loaded.", pkg, dsc$Version))
+##     }
 
-    # Load dll:
-    library.dynam("fNonlinear", pkg, lib)
-}
+##     # Load dll:
+##     library.dynam("fNonlinear", pkg, lib)
+## }
 
 
 if(!exists("Sys.setenv", mode = "function")) # pre R-2.5.0, use "old form"
@@ -50,4 +50,3 @@ if(!exists("Sys.setenv", mode = "function")) # pre R-2.5.0, use "old form"
 
 
 ################################################################################
-
